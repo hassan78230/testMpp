a36f38926dbd880d2799bc173dd767478b51ca30
krafat hassan <hakat2003@yahoo.fr>
2021-12-17 12:22:33 +0100

    ajout header

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,8 +7,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: const Center(
-        child: Text('Home Page'),
+      body: Column(
+        children: <Widget>[
+          _buildHeader(),
+        ],
+      ),
+    );
+  }
+
+  _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Monday,Dec 28',
+                style: TextStyle(
+                  color: Colors.grey[400]!,
+                ),
+              ),
+              const Text(
+                'Top Picks',
+                style: TextStyle(
+                    fontFamily: 'Philosopher',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32),
+              )
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: const Icon(
+              Icons.apps,
+              size: 40,
+            ),
+          )
+        ],
       ),
     );
   }
